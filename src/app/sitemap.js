730b6461e1788ce97440ec85cacf22af083f66aa
Kelly Martin <kellymartin@Kellys-MacBook-Pro.local>
2026-06-29 13:50:15 -0700

    Add sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,13 @@
+export default function sitemap() {
+  return [
+    { url: 'https://www.varianttraininglab.com', lastModified: new Date() },
+    { url: 'https://www.varianttraininglab.com/training', lastModified: new Date() },
+    { url: 'https://www.varianttraininglab.com/physical-therapy', lastModified: new Date() },
+    { url: 'https://www.varianttraininglab.com/exclusive-amenities', lastModified: new Date() },
+    { url: 'https://www.varianttraininglab.com/health', lastModified: new Date() },
+    { url: 'https://www.varianttraininglab.com/athletes', lastModified: new Date() },
+    { url: 'https://www.varianttraininglab.com/membership', lastModified: new Date() },
+    { url: 'https://www.varianttraininglab.com/schedule', lastModified: new Date() },
+    { url: 'https://www.varianttraininglab.com/contact', lastModified: new Date() },
+  ]
+}
